f601963a2f5594c032a0772ea200f11e58607c3a
connordennison <connor@dennison.email>
2020-12-21 17:02:41 +0000

    fixed the bug where it didn't indefinitively loop, v1.1 i guess?

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ const {
 } = require("./config.json")
 const client = new Discord.Client()
 
+
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
     const voiceChannel = client.channels.cache.get(channel_id)
     voiceChannel.join().then(connection => {
         console.log("Joined voice channel")
-        const dispatcher = connection.play(ytdl(video_urls[Math.floor(Math.random() * video_urls.length)], { filter: "audioonly" }))
-        dispatcher.on("finish", handler => {
-            connection.play(ytdl(video_urls[Math.floor(Math.random() * video_urls.length)], { filter: "audioonly" }))
-        })
-        dispatcher.on("error", err => {
-            console.log(err)
-        })
+        function play(connection) {
+            const stream = ytdl(video_urls[Math.floor(Math.random() * video_urls.length)], { filter: "audioonly" })
+            const dispatcher = connection.play(stream)
+            dispatcher.on("finish", () => {
+                play(connection)
+            })
+        }
+
+        play(connection)
     })
 })
 
