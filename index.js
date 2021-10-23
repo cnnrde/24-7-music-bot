157f77eb73a45f40a61c1f64764554360c3ad187
Scoooolz <84376629+Scoooolz@users.noreply.github.com>
2021-10-23 15:18:13 +0700

    Support 24/7 with uptimerobot.com

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-const Discord = require("discord.js")
-const ytdl = require("ytdl-core")
+const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
 const {
     token,
     channel_id,
     video_urls
-} = require("./config.json")
-const client = new Discord.Client()
+} = require("./config.json");
+const client = new Discord.Client();
+const port = 3000;
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+    res.send('Looks your bot is online!');
+});
+
+app.listen(port);
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-    const voiceChannel = client.channels.cache.get(channel_id)
+    console.log(`Logged in as ${client.user.tag}`);
+    const voiceChannel = client.channels.cache.get(channel_id);
     voiceChannel.join().then(connection => {
-        console.log("Joined voice channel")
+        console.log("Joined voice channel");
         function play(connection) {
-            const stream = ytdl(video_urls[Math.floor(Math.random() * video_urls.length)], { filter: "audioonly" })
-            const dispatcher = connection.play(stream)
+            const stream = ytdl(video_urls[Math.floor(Math.random() * video_urls.length)], { filter: "audioonly" });
+            const dispatcher = connection.play(stream);
             dispatcher.on("finish", () => {
-                play(connection)
-            })
-        }
+                play(connection);
+            });
+        };
 
-        play(connection)
-    })
-})
+        play(connection);
+    });
+});
 
-client.login(token)
+client.login(token);
