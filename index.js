4277861b71cea7a2a2e61440d6e0305ef61226e7
Connor Dennison <me@cnnd.co.uk>
2021-11-07 14:49:29 +0000

    🧰 | updates make code prettier make webserver optional add to readme make .replit install packages move token to .env

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-const Discord = require("discord.js");
-const ytdl = require("ytdl-core");
-const {
-    token,
-    channel_id,
-    video_urls
-} = require("./config.json");
-const client = new Discord.Client();
-const port = 3000;
-const express = require("express");
-const app = express();
+// set up dotenv
+require('dotenv').config()
+const Discord = require('discord.js')
+const ytdl = require('ytdl-core')
+const { channel_id, video_urls, webserver } = require('./config.json')
+const client = new Discord.Client()
+const port = 3000
+const express = require('express')
+const app = express()
 
-app.get("/", (req, res) => {
-    res.send('Looks your bot is online!');
-});
+if (webserver) {
+  app.get('/', (req, res) => {
+    res.send('pong')
+  })
 
-app.listen(port);
+  app.listen(port)
+}
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`);
-    const voiceChannel = client.channels.cache.get(channel_id);
-    voiceChannel.join().then(connection => {
-        console.log("Joined voice channel");
-        function play(connection) {
-            const stream = ytdl(video_urls[Math.floor(Math.random() * video_urls.length)], { filter: "audioonly" });
-            const dispatcher = connection.play(stream);
-            dispatcher.on("finish", () => {
-                play(connection);
-            });
-        };
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`)
+  const voiceChannel = client.channels.cache.get(channel_id)
+  voiceChannel.join().then((connection) => {
+    console.log('Joined voice channel')
+    function play(connection) {
+      const stream = ytdl(
+        video_urls[Math.floor(Math.random() * video_urls.length)],
+        { filter: 'audioonly' },
+      )
+      const dispatcher = connection.play(stream)
+      dispatcher.on('finish', () => {
+        play(connection)
+      })
+    }
 
-        play(connection);
-    });
-});
+    play(connection)
+  })
+})
 
-client.login(token);
+client.login(process.env.TOKEN)
